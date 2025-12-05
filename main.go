ce0a823db513b5890e0d2a1ce89fb938b4b0d548
frangi01 <francesco.ditonno00@gmail.com>
2025-12-05 15:56:37 +0100

    feat: setwebhook

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/mymmrac/telego"
 )
 
 // handler per /api (per ora dummy)
@@ -20,6 +24,42 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 func telegramHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write([]byte("OK"))
+}
+
+func telegramWebHookHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	botToken := "7915593277:AAFXsBGXij-dsXDNLMb6Orb8zyNXw7fHE7c"
+
+	// Note: Please keep in mind that default logger may expose sensitive information,
+	// use in development only
+	bot, err := telego.NewBot(botToken, telego.WithDefaultDebugLogger())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Set up a webhook on Telegram side
+	_ = bot.SetWebhook(ctx, &telego.SetWebhookParams{
+		URL:         "https://crecs-bot.it/telegram",
+		SecretToken: bot.SecretToken(),
+	})
+
+	// Receive information about webhook
+	info, _ := bot.GetWebhookInfo(ctx)
+	if err != nil {
+		http.Error(w, "failed to get webhook info", http.StatusInternalServerError)
+		return
+	}
+
+	b, err := json.Marshal(info)
+	if err != nil {
+		http.Error(w, "failed to marshal info", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(b)
 }
 
 func main() {
@@ -37,6 +77,8 @@ func main() {
 
 	// --- ROUTA TELEGRAM WEBHOOK ---
 	mux.HandleFunc("/telegram", telegramHandler)
+
+	mux.HandleFunc("/telegramWebHook", telegramWebHookHandler)
 	// o /telegram/webhook se vuoi qualcosa di più specifico
 	// mux.HandleFunc("/telegram/webhook", telegramHandler)
 
